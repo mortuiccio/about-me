fe1bf047dfc15ec76433798c47dfbf6cd5d6f329
mortuiccio <keithmortuiccio@gmail.com>
2018-06-05 18:39:56 -0700

    about me is working and finshed

---
+++ app.js
@@ -0,0 +1,61 @@
+'use strict';
+
+alert('It is time for you to know about me!');
+
+var userName = prompt('what is your name?');
+
+console.log("the user is: ", userName);
+
+var daughter = prompt('Is my daughters name Tabitha? Please answer all questions with Y for yes and N for no.').toLowerCase();
+
+console.log('daughter', daughter);
+
+if (daughter === 'y') {
+  alert('you are correct');
+} else {
+
+  alert('nice try but incorrect');
+}
+
+var Taya = prompt('Is my dogs name Taya? Please answer all questions with Y for yes and N for no.').toLowerCase();
+
+console.log('Taya', Taya);
+
+if (Taya === 'y') {
+  alert('you are correct');
+} else {
+  alert('nice try but incorrect');
+}
+
+var trevor = prompt('Is my cats name Trevor? Please answer all questions with Y for yes and N for no.').toLowerCase();
+
+console.log('Trevor', trevor);
+
+if (trevor === 'n') {
+  alert('you are correct');
+} else {
+
+  alert('nice try but incorrect');
+}
+
+var ben = prompt('Is my friends name Ben? Please answer all questions with Y for yes and N for no.').toLowerCase();
+
+console.log('Ben', ben);
+
+if (ben === 'y') {
+  alert('you are correct');
+} else {
+
+  alert('nice try but incorrect');
+}
+var wife = prompt('Is my wife dead? Please answer all questions with Y for yes and N for no.').toLowerCase();
+
+console.log('wife', wife);
+
+if (wife === 'y') {
+  alert('you are correct');
+} else {
+
+  alert('nice try but incorrect');
+}
+
