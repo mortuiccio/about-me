66298b88bfb65292e03d344ffea88460e19f0ea6
mortuiccio <keithmortuiccio@gmail.com>
2018-06-14 02:27:18 -0700

    finished

--- app.js
+++ app.js
@@ -5,73 +5,95 @@ alert('It is time for you to know about me!');
 var userName = prompt('what is your name?');
 
 console.log("the user is: ", userName);
+function question1(){
 
-var daughter = prompt('Is my daughters name Tabitha? Please answer all questions with Y for yes and N for no.').toLowerCase();
 
-console.log('daughter', daughter);
+  var daughter = prompt('Is my daughters name Tabitha? Please answer all questions with Y for yes and N for no.').toLowerCase();
 
-if (daughter === 'y') {
+  console.log('daughter', daughter);
+
+  if (daughter === 'y') {
     points++;
-  alert('you are correct and you have '+ points + ' points');
-} else {
+    alert('you are correct and you have '+ points + ' points');
+  } else {
 
-  alert('nice try but incorrect and you did not get a point, you have '+ points + ' points');
+    alert('nice try but incorrect and you did not get a point, you have '+ points + ' points');
+  }
 }
+question1();
 
-var Taya = prompt('Is my dogs name Taya? Please answer all questions with Y for yes and N for no.').toLowerCase();
+function question2(){
 
-console.log('Taya', Taya);
 
-if (Taya === 'y') {
+  var Taya = prompt('Is my dogs name Taya? Please answer all questions with Y for yes and N for no.').toLowerCase();
+
+  console.log('Taya', Taya);
+
+  if (Taya === 'y') {
     points++; 
-  alert('you are correct and you have '+ points + ' points');
-} else {
-  alert('nice try but incorrect');
+    alert('you are correct and you have '+ points + ' points');
+  } else {
+    alert('nice try but incorrect');
+  }
 }
+question2();
 
-var trevor = prompt('Is my cats name Trevor? Please answer all questions with Y for yes and N for no.').toLowerCase();
+function question3(){
 
-console.log('Trevor', trevor);
+  var trevor = prompt('Is my cats name Trevor? Please answer all questions with Y for yes and N for no.').toLowerCase();
 
-if (trevor === 'n') {
+  console.log('Trevor', trevor);
+
+  if (trevor === 'n') {
     points++;
-  alert('you are correct and you have '+ points + ' points');
-} else {
+    alert('you are correct and you have '+ points + ' points');
+  } else {
 
-  alert('nice try but incorrect');
+    alert('nice try but incorrect');
+  }
 }
+question3();
 
-var ben = prompt('Is my friends name Ben? Please answer all questions with Y for yes and N for no.').toLowerCase();
+function question4(){
 
-console.log('Ben', ben);
+  var ben = prompt('Is my friends name Ben? Please answer all questions with Y for yes and N for no.').toLowerCase();
 
-if (ben === 'y') {
+  console.log('Ben', ben);
+
+  if (ben === 'y') {
     points++;
-  alert('you are correct and you have '+ points + ' points');
-} else {
+    alert('you are correct and you have '+ points + ' points');
+  } else {
 
-  alert('nice try but incorrect');
+    alert('nice try but incorrect');
+  }
 }
-var wife = prompt('Is my wife dead? Please answer all questions with Y for yes and N for no.').toLowerCase();
+question4();
 
-console.log('wife', wife);
+function question5(){
 
-if (wife === 'y') {
+  var wife = prompt('Is my wife dead? Please answer all questions with Y for yes and N for no.').toLowerCase();
+
+  console.log('wife', wife);
+
+  if (wife === 'y') {
     points++;
-  alert('you are correct and you have '+ points + ' points');
-} else {
+    alert('you are correct and you have '+ points + ' points');
+  } else {
 
-  alert('nice try but incorrect');
+    alert('nice try but incorrect');
+  }
 }
+question5();
 
 function question6(){
-  var chances = 4
+  var chances = 4;
 
   var answer = prompt('how many kids do you think I have?');
-console.log(answer, chances)
+  console.log(answer, chances);
   while (chances > 0) {
     if (answer === '1'){
-        points++;
+      points++;
       return alert('correct and you have '+ points + ' points');
     
     }
@@ -79,29 +101,29 @@ console.log(answer, chances)
       answer = prompt ('Incorrect you are to low')    
       chances --;
     }else { 
-        answer = prompt ('Incorrect to high')
-        chances--
+      answer = prompt ('Incorrect to high')
+      chances--;
     }
   }
 }
 
-question6()
+question6();
 
 function question7(){
-    var chances = 6
+  var chances = 6
   
-    var answer = prompt('Guess what countries I have been too?').toLowerCase();
+  var answer = prompt('Guess what countries I have been too?').toLowerCase();
   console.log(answer, chances)
-    while (chances > 0) {
-      if (answer === 'iraq'|| answer === 'kuwait'){
-        points++;
-        return alert('your awesome you got it and you have '+ points + ' points');
-      }else { 
-          answer = prompt ('nice try but you are so wrong').toLowerCase();
-          chances--;
-      }
+  while (chances > 0) {
+    if (answer === 'iraq'|| answer === 'kuwait'){
+      points++;
+      return alert('your awesome you got it and you have '+ points + ' points');
+    }else { 
+      answer = prompt ('nice try but you are so wrong').toLowerCase();
+      chances--;
     }
   }
+}
 
 question7();
 
